e792621f3a4134ca42e2b90a3ed0ca3e1180d6e4
Gel <gel@leandro@alertaapp.com.br>
2018-03-31 21:33:44 -0300

    trying to make post

--- src/clojure_api/handler.clj
+++ src/clojure_api/handler.clj
@@ -13,15 +13,28 @@
 (def task-query "MATCH (task:Task) RETURN task"
   )
 
+(def insert-query
+  [desc, status]
+  (nr/create conn {:desc desc, :status status})
+ )
+
 (defn get-tasks
   []
   (let [result (cy/tquery conn task-query)]
     (map (fn [x] {:test x}) result))
  )
 
+(defn insert-task
+  [description,
+   status]
+  (let [result (cy/tquery conn insert-query {:desc description, :status status})]
+   )
+ )
+
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (GET "/tasks" [] (resp/response (get-tasks)))
+  (POST "/tasks" [description status :as request] (resp/response (insert-task (:params request))))
   (route/not-found "Not Found"))
 
 
